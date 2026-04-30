020e05881dbd6cc2f5b71e01256883783c9265c3
aylinsarioglu <sariogluadu55@gmail.com>
2026-04-30 23:32:56 +0300

    feat: add basic food dataset for suggestions

---
+++ foods.ts
@@ -0,0 +1,12 @@
+export type FoodItem = {
+  name: string
+  calories: number
+}
+
+export const FOOD_DATA: FoodItem[] = [
+  { name: "makarna", calories: 350 },
+  { name: "tavuk pilav", calories: 450 },
+  { name: "pizza", calories: 700 },
+  { name: "salata", calories: 150 },
+  { name: "yumurta", calories: 80 },
+]
